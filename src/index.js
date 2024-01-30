8ced9715e4626d562b0cbb2e3e661ca5b9a3a898
Bodi_Ali <bodiali59@gmail.com>
2024-01-30 12:28:37 +0200

    Add webpack configuration settings and finish layout

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import homeContent from "./home.js";
+import menuContent from "./menu.js";
+import "./reset.css"
+import "./style.css"
+
+document.addEventListener("DOMContentLoaded", homeContent)
+
