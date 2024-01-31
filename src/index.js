18a64606e60760e45ad17c53f6b41e11c1c9d073
Bodi_Ali <bodiali59@gmail.com>
2024-01-31 08:43:47 +0200

    Add an icon

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import menuContent from "./menu.js";
 import aboutContent from "./about.js";
 import "./reset.css"
 import "./style.css"
+import icon from "./images/potato.png"
+
+// Add an icon
+const linkTag = document.createElement("link")
+linkTag.rel = "icon"
+linkTag.href = icon
+head.appendChild(linkTag)
 
 const button1 = document.querySelector("button:nth-of-type(1)")
 const button2 = document.querySelector("button:nth-of-type(2)")
 const button3 = document.querySelector("button:nth-of-type(3)")
+const head = document.querySelector("head")
+
 document.addEventListener("DOMContentLoaded", homeContent)
 button1.addEventListener("click", generateHome)
 button2.addEventListener("click",generateMenu)
